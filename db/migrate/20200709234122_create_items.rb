b0668740ac1b88fcc636ad433ddc39c2452e0e54
tomciv9000 <eugene.white@gmail.com>
2020-07-09 16:46:23 -0700

    Add default false value to complete boolean

--- db/migrate/20200709234122_create_items.rb
+++ db/migrate/20200709234122_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string :description
-      t.boolean :complete
+      t.boolean :complete, default: false
 
       t.timestamps
     end
