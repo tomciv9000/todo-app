49d8808d9b14fc0664feb763b13401483359dbb8
tomciv9000 <eugene.white@gmail.com>
2020-07-09 16:49:30 -0700

    Add scope routes

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_07_09_234122) do
 
   create_table "items", force: :cascade do |t|
     t.string "description"
-    t.boolean "complete"
+    t.boolean "complete", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
